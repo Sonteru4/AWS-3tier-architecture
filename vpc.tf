64674a6ad7af352af73b0614a3d87e1cbb824fcb
Sonteru4 <sanjanaonteru404@gmail.com>
2023-04-10 15:33:42 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
-  db_subnets  = ["10.0.5.0/24", "10.0.6.0/24"]
+  database_subnets  = ["10.0.5.0/24", "10.0.6.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
