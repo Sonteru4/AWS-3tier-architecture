77fedc31bf3671c6bd1dd8e2389c26af933e50e5
Sonteru4 <sanjanaonteru404@gmail.com>
2023-04-10 18:59:48 +0530

    Create bastion.tf

---
+++ bastion.tf
@@ -0,0 +1,16 @@
+module "ec2_public" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "BastionHost"
+ 
+  ami                    = data.aws_ami.amzlinux2.id
+  instance_type          = "t2.micro"
+  key_name               = "my-acc"
+ 
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags{
+    Name = "Bastion-3 tier"
+  }
+}
